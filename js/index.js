545390cd1abae7912ea1f1124c0e9ac1e1b1f966
fishmanjohn <john@emmaandjohnbyvan.com>
2019-10-29 19:55:11 -0700

    done

--- js/index.js
+++ js/index.js
@@ -20,11 +20,67 @@ letsGo.addEventListener('click', ()=>{
     letsGo.style.color ="red";
 })
 
-const mapChange = document.querySelector(".map-img")
+const mapChange = document.querySelector(".map-img");
+const body = document.querySelector("body");
 window.addEventListener("resize", ()=>{
     mapChange.src="img/neon.jpg";
-    window.style.
+    body.style.backgroundColor="teal";
 })
 
 console.log(mapChange)
 
+const destinationShift = document.querySelector(".content-destination h2");
+
+destinationShift.addEventListener('mouseenter', ()=>{
+    destinationShift.style.color = '#33ff00';
+})
+
+destinationShift.addEventListener('mouseleave', ()=>{
+    destinationShift.style.color = '#3c00ff';
+})
+
+const imgRnd = document.querySelector(".content-destination img");
+
+document.addEventListener("keypress", () => {
+   
+   imgRnd.style.borderRadius = "200px";
+});
+
+const textColor = document.querySelector(".content-section :last-child p")
+
+textColor.addEventListener('mousedown',()=>{
+    textColor.style.color = "#40ff00";
+});
+
+const buttonone = document.querySelector(".content-pick :first-child .btn ");
+
+    window.addEventListener('keyup',(event)=>{
+        buttonone.style.backgroundColor = "#ff0d00";
+        event.stopPropagation();
+    });
+
+    const buttontwo = document.querySelector(".content-pick :nth-child(2) .btn ");
+
+    window.addEventListener('keyup',(event)=>{
+        buttontwo.style.backgroundColor = "#ffea00";
+        event.stopPropagation();
+    });
+
+const buttonthree = document.querySelector(".content-pick :last-child .btn ");
+
+    window.addEventListener('keyup',(event)=>{
+        buttonthree.style.backgroundColor = "limegreen";
+        event.stopPropagation();
+    })
+
+    const tookPill = document.querySelector(".img-fluid")
+
+    tookPill.addEventListener('mousemove',()=>{
+        tookPill.src ="img/pill.jpg"
+    })
+
+
+    let navigation = document.querySelectorAll('.nav a');
+    navigation.forEach(title => {
+        navigation.preventDefault();
+        })
